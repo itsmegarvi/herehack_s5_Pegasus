90ef02e5ae4244804472a801dea1b019e73b99c3
Garvit Shubham <mailme.garvit@gmail.com>
2022-11-26 00:15:57 +0530

    feat add loco.html

--- maps/urls.py
+++ maps/urls.py
@@ -5,4 +5,5 @@ from maps import views
 urlpatterns = [
     path('', views.index, name='index_page'),
     path('map/', views.map, name='map_page'),
+    path('loco/', views.loco, name='loco_page')
 ]
